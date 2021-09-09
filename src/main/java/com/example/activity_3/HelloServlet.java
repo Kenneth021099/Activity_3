5679b3537edf9acbeec04fa9602b2b494a53e36b
Kenneth021099 <klmanalac@student.hau.edu.ph>
2021-09-09 16:05:12 +0800

    Activity3

--- src/main/java/com/example/activity_3/HelloServlet.java
+++ src/main/java/com/example/activity_3/HelloServlet.java
@@ -1,25 +1,23 @@
 package com.example.activity_3;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h3> Hello World!</h3>");
+        }
     }
 
     public void destroy() {
